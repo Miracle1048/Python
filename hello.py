58de999e2a69f4bbf87988e35ecea362f4c33979
Gabriel Miracle <miraclematthias1@gmail.com>
2024-08-12 17:55:58 +0100

    first commit

---
+++ hello.py
@@ -0,0 +1,38 @@
+import sys
+print(sys.version)
+
+#this is python class day 1
+"""
+multi line string comment
+multi line string comment
+multi line string comment
+multi line string comment
+"""
+print("result")
+
+x = str(10)
+y = 20
+z = "codeCampus"
+# = "Hello"
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x,y,z = "orange", "banana", "mango"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "cashew "
+print(x + y + z)
+
+W  = "my "
+X  = "name "
+Y  = "is "
+Z  = "Miarcle "
+print(W + X + Y + Z)
+
+car = "benz"
+
+text = "str"
